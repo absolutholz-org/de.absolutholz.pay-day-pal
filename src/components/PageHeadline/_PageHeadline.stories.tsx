e0ed6e494a6410db078e34ac7271406488722927
absolutholz <absolutholz@gmail.com>
2026-01-25 19:06:39 +0100

    Update PageHeadline

---
+++ src/components/PageHeadline/_PageHeadline.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+
+import { PageHeadline } from ".";
+
+const meta = {
+  title: "Components/PageHeadline",
+  component: PageHeadline,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof PageHeadline>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: "Page Headline",
+  },
+};
--- src/components/PageHeadline/_PageHeadline.styles.ts
+++ src/components/PageHeadline/_PageHeadline.styles.ts
@@ -1,8 +1,28 @@
+import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 
+import { colorSchemeCss } from "../../_colorSchemeCss";
+
 export const PageHeadline = styled.h1`
-  font-size: 2rem;
-  color: var(--on-surface);
-  margin: 0;
+  background-clip: text;
+  ${colorSchemeCss({
+    lightCssVars: css`
+      background-image: linear-gradient(
+        to right,
+        oklch(0.558 0.288 302.321) 0%,
+        oklch(0.592 0.249 0.584) 100%
+      );
+    `,
+    darkCssVars: css`
+      background-image: linear-gradient(
+        to right,
+        oklch(0.714 0.203 305.504) 0%,
+        oklch(0.718 0.202 349.761) 100%
+      );
+    `,
+  })}
+  color: transparent;
+  font-size: 1.875rem;
   font-weight: 700;
+  margin: 0;
 `;
