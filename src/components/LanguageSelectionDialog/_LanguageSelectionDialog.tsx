2cc2a02c94f179b28fce615683bf34d2695a085a
absolutholz <absolutholz@gmail.com>
2026-02-08 19:58:41 +0100

    feat: create language selection dialog for settings screen

--- src/components/LanguageSelectionDialog/_LanguageSelectionDialog.tsx
+++ src/components/LanguageSelectionDialog/_LanguageSelectionDialog.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import { SUPPORTED_CURRENCIES, SUPPORTED_LANGUAGES } from "../../constants";
+import { SUPPORTED_LANGUAGES } from "../../constants";
 import { useLocalization } from "../../context/LocalizationContext";
 import { Language } from "../../types";
 import { Dialog } from "../Dialog";
