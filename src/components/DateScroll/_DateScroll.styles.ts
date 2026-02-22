eaeb8ba766bdc4ddda5b1d8d4c0ba3cd7daf636f
absolutholz <absolutholz@gmail.com>
2026-02-22 13:00:53 +0100

    fix: add extra spacing to date scroll to show shadows

--- src/components/DateScroll/_DateScroll.styles.ts
+++ src/components/DateScroll/_DateScroll.styles.ts
@@ -3,5 +3,6 @@ import styled from "@emotion/styled";
 import { FullBleedScrollableContainer } from "../FullBleedScrollableContainer";
 
 export const DateScroll = styled(FullBleedScrollableContainer)`
-	margin-bottom: 2rem;
+	margin-block: -0.25rem -0.675rem; // Compensate for the spacing added for the drop shadow of the date cards plus a little extra to ensure the last card's shadow is fully visible
+	padding-block: 0.25rem 2.675rem; // Compensate for the drop shadow of the DateCards
 `;
